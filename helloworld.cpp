d40e08b6622661fca24caa2329d02fbb089eb728
aryahule <aryahule27@gmail.com>
2024-07-31 15:11:41 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+//Name - Arya Hule
+//PRN - 23070123029
+//Aim - Printing out Hello World
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    return 0;
+}
+/* Output:
+Hello World
+*/
